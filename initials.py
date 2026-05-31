2dacfc68bd425a5919d0d000c32c75219bd624ae
Glenn395 <glennyeboah395@gmail.com>
2026-05-31 21:35:40 +0000

    ASSIGNMENT -1 2425401778

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Glenn Yeboah
+# STUDENT ID: 2425401778
+# Email: scepticalgh395@gmail.com
+# Grader: Augustus
+#
+# On my honor, Glenn Yeboah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in normal and large format
 
+    print()
+    print("...GYK")
+    print()
+
+    print("...GGGGGGGGGGGG...YY........YY...KK........KK...****")
+    print("...GG........GG....YY......YY....KK......KK.....****")
+    print("...GG..............YY......YY....KK....KK.......****")
+    print("...GG...............YY....YY.....KK..KK.........****")
+    print("...GG....GGGGGG......YYYYYY......KKKK...........****")
+    print("...GG........GG........YY........KK..KK.........****")
+    print("...GG........GG........YY........KK....KK.......****")
+    print("...GG........GG........YY........KK......KK.....****")
+    print("...GG........GG........YY........KK........KK...****")
+    print("...GGGGGGGGGGGG........YY........KK..........KK.****")
+    print()
 
 
 main()
